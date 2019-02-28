5d368c2c0080a1ce5b71068102b33d5b72a8d534
zoealinn <47342624+zoealinn@users.noreply.github.com>
2019-02-28 15:03:01 -0700

    Add files via upload
    updated to have an ArrayList<PlayerChar> instead of a PlayerChar[]

---
+++ models/Player.java
@@ -0,0 +1,41 @@
+package models;
+
+import java.util.ArrayList;
+
+public class Player {
+	final String NAME;
+	private ArrayList<PlayerChar> chars;
+	//player at index 0 is the current character being played
+	private static int playerNum = 0;
+	
+	
+	public Player(String name) {
+		this.NAME = name;
+		
+	}
+	
+	public Player() {
+		playerNum+=1;
+		this.NAME = "Player " + playerNum;
+	}
+
+
+	public ArrayList<PlayerChar> getChars() {
+		return chars;
+	}
+
+	public void setChars(ArrayList<PlayerChar> chars) {
+		this.chars = chars;
+	}
+
+
+	@Override
+	public String toString() {
+		//TODO make decent-looking toString().
+		
+		StringBuilder builder = new StringBuilder();
+		builder.append("Player Name: ").append(NAME)
+		.append("\nAmount of characters: ").append(chars.size());
+		return builder.toString();
+	}
+}
