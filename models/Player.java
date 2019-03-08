d56d97f48138f324a2614481adc46d8a8adb57e9
LucasMcManus <lucasmcmanus21@gmail.com>
2019-03-08 13:23:33 -0700

    Still trying to fix

--- models/Player.java
+++ models/Player.java
@@ -7,10 +7,11 @@ public class Player {
 	private ArrayList<PlayerChar> chars;
 	//player at index 0 is the current character being played
 	private static int playerNum = 0;
-	
+	private int playerID = 0;
 	
 	public Player(String name) {
 		playerNum += 1;
+		setPlayerID(playerNum);
 		this.NAME = name;
 		chars = new ArrayList<>();
 		chars.add(new PlayerChar());
@@ -32,6 +33,9 @@ public class Player {
 		this.chars = chars;
 	}
 
+	public static int getPlayerNum() {
+		return playerNum;
+	}
 
 	@Override
 	public String toString() {
@@ -41,5 +45,13 @@ public class Player {
 		builder.append("Player Name: ").append(NAME)
 		.append("\nAmount of characters: ").append(getChars().size());
 		return builder.toString();
+	}
+
+	public int getPlayerID() {
+		return playerID;
+	}
+
+	public void setPlayerID(int playerID) {
+		this.playerID = playerID;
 	}
 }
--- views/RankUp.java
+++ views/RankUp.java
@@ -1,5 +1,6 @@
 package views;
 
+import controllers.Controller;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -10,10 +11,6 @@ import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 public class RankUp {
-	
-	public static void rankUpDuke() {
-		//logic to rank up to duke
-	}
 
 	public static void rankUpBoth() {
 		String name ="";
@@ -23,13 +20,16 @@ public class RankUp {
 		prompt.setTitle("Rank Up");
 		
 		Text label = new Text();
-		label.setText("Congrats! You now have at least 50 shekels in your possesion and at least 50 prestige to your name! Time to rank up");
+		label.setText("Congrats! You now have 500 shekels in your possesion and 500 prestige to your name! Time to rank up");
 		Button knight = new Button("Knight");
-		knight.setOnAction(e ->{
-			
-		});
+		knight.setOnAction(e -> Controller.rankUpKnight());
+		
 		Button priest = new Button("Priest");
+		priest.setOnAction(e -> Controller.rankUpPriest());
+		
 		Button merchant = new Button("Merchant");
+		merchant.setOnAction(e -> Controller.rankUpMerchant());
+		
 		Label question = new Label("Choose your path");
 		
 		HBox buttons = new HBox(20);
@@ -49,7 +49,7 @@ public class RankUp {
 		prompt.setTitle("Rank Up");
 		
 		Text label = new Text();
-		label.setText("Congrats! You now have at least 50 prestige to your name! Time to rank up");
+		label.setText("Congrats! You now have 500 prestige to your name! Time to rank up");
 		Button knight = new Button("Knight");
 		knight.setOnAction(e ->{
 			
@@ -67,26 +67,40 @@ public class RankUp {
 	}
 	
 	public static void rankUpShekels() {
-		String name ="";
+		Stage prompt = new Stage();
+		prompt.setTitle("Congrats Merchant!");
+		prompt.initModality(Modality.APPLICATION_MODAL);
+		
+		Text label = new Text();
+		label.setText("When you have more than 50 shekels, you become a merchant and gain 100 more shekels!");
+		Button ok = new Button("Awesome!");
+		ok.setOnAction(e -> {
+			Controller.rankUpMerchant();
+			prompt.close();
+		});
+		
+		VBox layout = new VBox(15);
+		Scene scene = new Scene(layout, 50, 300);
+		prompt.setScene(scene);
+		prompt.show();
+	}
+	
+	public static void rankUpDuke() {
 		Stage prompt = new Stage();
 		
 		prompt.initModality(Modality.APPLICATION_MODAL);
-		prompt.setTitle("Rank Up");
+		prompt.setTitle("On your way up to Duke!");
 		
 		Text label = new Text();
-		label.setText("Congrats! You now have at least 50 shekels to your name! Time to rank up");
-		Button duke = new Button("Duke");
+		label.setText("Congrats! You now have 500 shekels to your name! Time to rank up");
+		Button duke = new Button("Woah, awesome!");
 		duke.setOnAction(e ->{
-			
+			Controller.rankUpDuke();
+			prompt.close();
 		});
-		
-		Button merchant = new Button("Priest");
-		Label question = new Label("Choose your path");
-		
-		HBox buttons = new HBox(20);
-		buttons.getChildren().addAll(duke, merchant);
+
 		VBox layout = new VBox(15);
-		layout.getChildren().addAll(label, buttons, question);
+		layout.getChildren().addAll(label, duke);
 		Scene scene = new Scene(layout, 300, 300);
 		prompt.setScene(scene);
 		prompt.show();
