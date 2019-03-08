31bf52aceadde68c48a4befbf7bee0a1fdd0a3e6
LucasMcManus <lucasmcmanus21@gmail.com>
2019-03-08 13:13:54 -0700

    rever

---
+++ views/CardEffects.java
@@ -0,0 +1,29 @@
+package views;
+
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import models.ChanceCard;
+
+public class CardEffects {
+
+	public static void message(ChanceCard chanceCard) {
+		Stage stage = new Stage();
+		stage.initModality(Modality.APPLICATION_MODAL);
+		stage.setTitle("Card Effect");
+		
+		Text text = new Text();
+		text.setText(chanceCard.getEffectString());
+		Button button = new Button("Okay");
+		button.setOnAction(e -> stage.close());
+		
+		VBox layout = new VBox(15);
+		layout.getChildren().addAll(text, button);
+		Scene scene = new Scene(layout, 300, 100);
+		stage.setScene(scene);
+		stage.show();
+	}
+}
